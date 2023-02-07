1cf25f0681b346fcd89b5535030feb3150b68dfb
Blaze Slavkovski <Blazeslavkovski@gmail.com>
2023-02-07 17:28:55 -0500

    added mixins for tablet and desktop breakpoints

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -1,10 +1,13 @@
-
+import "./HomePage.scss"
 
 export default function HomePage(){
 
     return(
         <>
+            <div className="test"> 
             This is the homepage
+            </div>
+            
         </>
     )
 }
