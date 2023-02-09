3bb5fa2e72fb06a7f4c4334033ea07b78a98338a
Blaze Slavkovski <Blazeslavkovski@gmail.com>
2023-02-08 21:44:40 -0500

    testing axios requests for frontend data

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -1,11 +1,45 @@
 import "./HomePage.scss"
+import axios from "axios"
+import {useState} from "react"
+import { useEffect } from "react";
 
 export default function HomePage(){
+    const [data, setData] = useState();
+    
+    
+    
 
+    useEffect(()=>{
+
+        async function grabData(){
+            try{
+                let {data} = await axios.get("http://localhost:8080/")
+                setData(data)
+            }catch{
+                console.log("Error 404")
+            }
+        }
+
+        grabData()
+    },[])
+
+    if(!data){
+        return <div>loading....</div>
+    }
+    console.log(data)
+
+    const races = data.map((cir) =>(
+        <>
+            <img className="image" src={cir.ActionImage}></img>
+            <img className="image" src={cir.CircuitImage}></img>
+        </>
+        
+    ))
     return(
         <>
             <div className="test"> 
             This is the homepage
+                {races}
             </div>
             
         </>
